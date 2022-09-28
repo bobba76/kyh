c21dd6e6be85e7527f7ece8f5a83d537f6380930
Robin Lindgren <rlindgren8@gmail.com>
2022-09-28 12:33:41 +0200

    Font and theme added

--- profilsida/app/src/main/java/com/bobba76/profilsida/MainActivity.java
+++ profilsida/app/src/main/java/com/bobba76/profilsida/MainActivity.java
@@ -2,13 +2,22 @@ package com.bobba76.profilsida;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        try
+        {
+            this.getSupportActionBar().hide();
+        }
+        catch (NullPointerException e){}
+
         setContentView(R.layout.activity_main);
     }
 }
