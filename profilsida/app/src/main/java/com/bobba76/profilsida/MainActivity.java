85645c30b29a39417d87a6bf355ab663c9a008b3
Robin Lindgren <rlindgren8@gmail.com>
2022-09-28 09:39:51 +0200

    Profilsida

---
+++ profilsida/app/src/main/java/com/bobba76/profilsida/MainActivity.java
@@ -0,0 +1,14 @@
+package com.bobba76.profilsida;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
